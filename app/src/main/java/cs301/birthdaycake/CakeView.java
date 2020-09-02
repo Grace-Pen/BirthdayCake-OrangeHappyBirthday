4a27c9121bddc82a0fe1ce1500ec1c06d84e7b5a
gracepenunuri <penunuri22@up.edu>
2020-09-02 16:42:07 -0700

    Switch and Candle Commit- Grace Penunuri

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -85,6 +85,7 @@ public class CakeView extends SurfaceView {
         float wickTop = bottom - wickHeight - candleHeight;
         canvas.drawRect(wickLeft, wickTop, wickLeft + wickWidth, wickTop + wickHeight, wickPaint);
 
+
     }
 
     /**
@@ -121,6 +122,9 @@ public class CakeView extends SurfaceView {
 
         //Now a candle in the center
         drawCandle(canvas, cakeLeft + cakeWidth/2 - candleWidth/2, cakeTop);
+        //draw another candle
+        drawCandle(canvas, cakeLeft + cakeWidth/4 - candleWidth/4, cakeTop);
+
 
     }//onDraw
 
